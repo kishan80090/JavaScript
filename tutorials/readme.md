89db450073b91fbb89f567b6bb15e1da74a914c8
Champak's World <champaksworld@gmail.com>
2025-10-18 14:42:00 +0530

    Showing current word  and adding to div

--- tutorials/readme.md
+++ tutorials/readme.md
@@ -2,3 +2,6 @@ Tested the wait function.
 
 It works
 ![alt text](image.png)
+
+
+![alt text](image-1.png)
--- tutorials/tester.js
+++ tutorials/tester.js
@@ -1,11 +1,22 @@
-
+const displayDiv = document.getElementById('dd');
+const totaldiv = document.getElementById('currentdiv');
 function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-function showWord(word, className) {
-  const displayDiv = document.getElementById('dd');
+function addWord(word, className) {
+  
   const p = document.createElement('p');
   p.className = className;
   p.textContent = word;
   displayDiv.appendChild(p);
 }
+ async function showOneWord(sentence) {
+      
+      let words = sentence.split(" ");
+      for (let word of words) {
+        totaldiv.innerHTML = word;
+        displayDiv.innerHTML = `${displayDiv.innerHTML} ${word}`;
+        await wait(5000);
+
+      }
+    }
