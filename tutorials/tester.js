3fe3db92c6559e9b2c200581b63a71044ea15c68
Champak's World <champaksworld@gmail.com>
2025-10-18 14:29:37 +0530

    Single word from a sentence is highlighted and being shown

--- tutorials/tester.js
+++ tutorials/tester.js
@@ -2,3 +2,10 @@
 function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+function showWord(word, className) {
+  const displayDiv = document.getElementById('dd');
+  const p = document.createElement('p');
+  p.className = className;
+  p.textContent = word;
+  displayDiv.appendChild(p);
+}
